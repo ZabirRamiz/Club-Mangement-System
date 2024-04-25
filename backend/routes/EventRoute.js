34ceebe41ad332dfcce8d49598c6a34744e38687
Zabir Ramiz <zabir.ramiz@g.bracu.ac.bd>
2024-04-25 22:03:32 +0600

    past events, upcoming events

--- backend/routes/EventRoute.js
+++ backend/routes/EventRoute.js
@@ -4,15 +4,20 @@ const {
     getSingleEvent,
     createEvent,
     deleteEvent,
-    updateEvent
+    updateEvent,
+    upcomingEvents,
+    pastEvents
 } = require("../controllers/EventModelController")
 
 const router = express.Router()
 
 router.get("/getEvents", getEvents)
 router.get("/getSingleEvent/:id", getSingleEvent)
+router.get("/getUpcomingEvents",upcomingEvents)
+router.get("/getPastEvents",pastEvents)
 router.post("/createEvent", createEvent)
 router.patch("/updateEvent/:id", updateEvent)
 router.delete("/deleteEvent/:id", deleteEvent)
+
 
 module.exports = router
