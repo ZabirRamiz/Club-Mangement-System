d058ebbf4908a309c3a01f6f55777ea375a04ada
aaditx23 <adit.pranto@gmail.com>
2024-04-27 10:04:21 +0600

    no image

--- frontend/src/components/UserEditProfile.jsx
+++ frontend/src/components/UserEditProfile.jsx
@@ -12,7 +12,7 @@ import { useNavigate } from "react-router-dom";
   const [confirmPassword, setConfirmPassword]= useState("")
   const [designation, setDesignation] = useState("");
   const [department, setDepartment] =useState("")
-  const [profileImage, setProfileImage] = useState(null);
+
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
@@ -251,17 +251,7 @@ import { useNavigate } from "react-router-dom";
                   {...(designation==="admin")? {disabled: false}: {disabled:true}}
                 />
               </div>
-              <div className="mb-6">
-                <label htmlFor="profileImage" className="block text-gray-800 font-bold mb-2">
-                  Profile Image
-                </label>
-                <input
-                  type="file"
-                  id="profileImage"
-                  onChange={(e) => setProfileImage(e.target.files[0])}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-              </div>
+              
               
               <button
                 className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
